d6a36b3184c64dd54aae510d23af9739c0706109
myall1977 <myall1977@gmail.com>
2018-03-14 10:58:43 +0900

    Update kospi200.py

--- kospi200.py
+++ kospi200.py
@@ -1,7 +1,11 @@
 import json
 from urllib.request import urlopen
 
-def tradePrice():
+def tradeprice():
 	base_url = 'http://stock.kakao.com/api/securities/KOREA-D2011029.json'
 	fin = urlopen(base_url).read().decode('utf-8')
-	return json.loads(fin).get('recentSecurity').get('tradePrice')
+	j_data = json.loads(fin)
+	tradePrice = j_data.get('recentSecurity').get('tradePrice')
+	signedChangePrice = j_data.get('recentSecurity').get('signedChangePrice')
+	data = '{} {}'.format(tradePrice,signedChangePrice)
+	return data
